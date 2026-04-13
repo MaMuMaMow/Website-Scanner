48869dc81e1e232ecfc2840f1ab625da9747a6cc
MaMuMaMow <166376767+MaMuMaMow@users.noreply.github.com>
2026-04-13 11:50:07 +0700

    planning

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+/*
+1 find how request look likes
+2 find parameters
+3 check each vulnerabilities
+	1 sql injection
+	2 missing header
+	3 file up misconfig
+	4 SSRF
+	5 redirect
+
+*/
+
+
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.
+	Println("test")
+
 }
